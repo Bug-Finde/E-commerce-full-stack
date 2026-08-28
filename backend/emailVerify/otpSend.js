b5057cf43532170bc7ec0eb643383b5b438d2a98
Muhammad Zeeshan Rafiq <zeeshanrafiq250@gmail.com>
2026-08-28 12:47:25 +0500

    code push

--- backend/emailVerify/otpSend.js
+++ backend/emailVerify/otpSend.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
   port: Number(process.env.SMTP_PORT) || 465,
   secure: (process.env.SMTP_SECURE || "true") === "true",
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
